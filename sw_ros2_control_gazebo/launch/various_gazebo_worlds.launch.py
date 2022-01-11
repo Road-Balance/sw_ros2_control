62570c541c0cdfbed71e11753798c4b08d41b128
kimsooyoung <tge1375@naver.com>
2022-01-12 00:23:34 +0900

    Feature : various worlds added

---
+++ sw_ros2_control_gazebo/launch/various_gazebo_worlds.launch.py
@@ -0,0 +1,49 @@
+import os
+import xacro
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.event_handlers import OnProcessExit
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.actions import IncludeLaunchDescription, ExecuteProcess, RegisterEventHandler
+
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+from osrf_pycommon.terminal_color import ansi
+
+
+def generate_launch_description():
+
+    world_file_name = "cafe.world"
+    
+
+    gazebo_model_path = os.path.join(get_package_share_directory('sw_ros2_control_gazebo'), 'models')
+
+    if 'GAZEBO_MODEL_PATH' in os.environ:
+        os.environ['GAZEBO_MODEL_PATH'] += ":" + gazebo_model_path
+    else :
+        os.environ['GAZEBO_MODEL_PATH'] = gazebo_model_path
+    print(ansi("yellow"), "If it's your 1st time to download Gazebo model on your computer, it may take few minutes to finish.", ansi("reset"))
+
+    pkg_path = os.path.join(get_package_share_directory('sw_ros2_control_gazebo'))
+    world_path = os.path.join(pkg_path, 'worlds', world_file_name)
+
+    pkg_gazebo_ros = FindPackageShare(package='gazebo_ros').find('gazebo_ros')   
+
+    # Start Gazebo server
+    start_gazebo_server_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(os.path.join(pkg_gazebo_ros, 'launch', 'gzserver.launch.py')),
+        launch_arguments={'world': world_path}.items()
+    )
+
+    # Start Gazebo client    
+    start_gazebo_client_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(os.path.join(pkg_gazebo_ros, 'launch', 'gzclient.launch.py'))
+    )
+
+    return LaunchDescription([
+        start_gazebo_server_cmd,
+        start_gazebo_client_cmd,
+    ])
